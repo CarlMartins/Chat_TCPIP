d7d1330594cca372864a32a9fab4669d630f6a30
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-04-10 17:41:31 -0300

    Commit inicial

---
+++ Chat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chat_TCPIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
